9152d2547fd4e9785cc37d0ec2dbffa951286918
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-14 07:09:02 +0000

    updated modules

--- library/azure_rm_mysqldatabase.py
+++ library/azure_rm_mysqldatabase.py
@@ -167,9 +167,9 @@ class AzureRMDatabases(AzureRMModuleBase):
                 self.to_do = Actions.Delete
             elif self.state == 'present':
                 self.log("Need to check if MySQL Database instance has to be deleted or may be updated")
-                if (self.collation is not None) and (self.collation != old_response['collation']):
+                if ('collation' in self.parameters) and (self.parameters['collation'] != old_response['collation']):
                     self.to_do = Actions.Update
-                if (self.charset is not None) and (self.charset != old_response['charset']):
+                if ('charset' in self.parameters) and (self.parameters['charset'] != old_response['charset']):
                     self.to_do = Actions.Update
                 self.delete_mysqldatabase()
 
--- library/azure_rm_postgresqldatabase.py
+++ library/azure_rm_postgresqldatabase.py
@@ -167,11 +167,12 @@ class AzureRMDatabases(AzureRMModuleBase):
                 self.to_do = Actions.Delete
             elif self.state == 'present':
                 self.log("Need to check if PostgreSQL Database instance has to be deleted or may be updated")
-                if (self.collation is not None) and (self.collation != old_response['collation']):
+                if ('collation' in self.parameters) and (self.parameters['collation'] != old_response['collation']):
                     self.to_do = Actions.Update
-                if (self.charset is not None) and (self.charset != old_response['charset']):
+                if ('charset' in self.parameters) and (self.parameters['charset'] != old_response['charset']):
                     self.to_do = Actions.Update
-                self.delete_postgresqldatabase()
+                if self.to_do == Actions.Update:
+                    self.delete_mysqldatabase()
 
         if (self.to_do == Actions.Create) or (self.to_do == Actions.Update):
             self.log("Need to Create / Update the PostgreSQL Database instance")
