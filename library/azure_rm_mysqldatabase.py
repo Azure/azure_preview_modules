628f56b0c5e8f9126aeb718c72baf81d51a86b69
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-14 07:24:09 +0000

    updated modules

--- library/azure_rm_mysqldatabase.py
+++ library/azure_rm_mysqldatabase.py
@@ -171,7 +171,8 @@ class AzureRMDatabases(AzureRMModuleBase):
                     self.to_do = Actions.Update
                 if ('charset' in self.parameters) and (self.parameters['charset'] != old_response['charset']):
                     self.to_do = Actions.Update
-                self.delete_mysqldatabase()
+                if self.to_do == Actions.Update:
+                    self.delete_mysqldatabase()
 
         if (self.to_do == Actions.Create) or (self.to_do == Actions.Update):
             self.log("Need to Create / Update the MySQL Database instance")
