5ae812d1d080c3c306aff86dc111b6c91d08d0b2
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-23 03:36:07 +0000

    updated modules

--- library/azure_rm_sqldatabase.py
+++ library/azure_rm_sqldatabase.py
@@ -70,21 +70,21 @@ options:
             - 'restore_long_term_retention_backup'
     source_database_id:
         description:
-            - "Conditional. If I(create_mode) is C(copy), C(non_readable_secondary), C(online_secondary), C(point_in_time_restore), C(recovery), or C(restore
+            - "Required if I(create_mode) is C(copy), C(non_readable_secondary), C(online_secondary), C(point_in_time_restore), C(recovery), or C(restore
               ), then this value is required. Specifies the resource ID of the source database. If I(create_mode) is C(non_readable_secondary) or C(online_s
               econdary), the name of the source database must be the same as the new database being created."
     source_database_deletion_date:
         description:
-            - "Conditional. If I(create_mode) is C(restore) and I(source_database_id) is the deleted database's original resource id when it existed (as oppo
+            - "Required if I(create_mode) is C(restore) and I(source_database_id) is the deleted database's original resource id when it existed (as oppo
               sed to its current restorable dropped database id), then this value is required. Specifies the time that the database was deleted."
     restore_point_in_time:
         description:
-            - "Conditional. If I(create_mode) is C(point_in_time_restore), this value is required. If I(create_mode) is C(restore), this value is optional. S
+            - "Required if I(create_mode) is C(point_in_time_restore), this value is required. If I(create_mode) is C(restore), this value is optional. S
               pecifies the point in time (ISO8601 format) of the source database that will be restored to create the new database. Must be greater than or e
               qual to the source database's earliestRestoreDate value."
     recovery_services_recovery_point_resource_id:
         description:
-            - "Conditional. If I(create_mode) is C(restore_long_term_retention_backup), then this value is required. Specifies the resource ID of the C(recov
+            - "Required if I(create_mode) is C(restore_long_term_retention_backup), then this value is required. Specifies the resource ID of the C(recov
               ery) point to C(restore) from."
     edition:
         description:
@@ -161,6 +161,13 @@ options:
       description:
           - Needs to be set to True in order to SQL Database to be updated.
       type: bool
+    state:
+      description:
+        - Assert the state of the SQL Database. Use 'present' to create or update an SQL Database and 'absent' to delete it.
+      default: present
+      choices:
+        - absent
+        - present
 
 extends_documentation_fragment:
     - azure
@@ -177,6 +184,27 @@ EXAMPLES = '''
       server_name: sqlcrudtest-5961
       name: testdb
       location: eastus
+
+  - name: Restore SQL Database
+    azure_rm_sqldatabase:
+      resource_group: sqlcrudtest-4799
+      server_name: sqlcrudtest-5961
+      name: restoreddb
+      location: eastus
+      create_mode: restore
+      restorable_dropped_database_id: "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/Default-SQL-SouthEastAsia/providers/Microsoft.Sql/s
+                                      ervers/testsvr/restorableDroppedDatabases/testdb2,131444841315030000"
+
+  - name: Create SQL Database in Copy Mode
+    azure_rm_sqldatabase:
+      resource_group: sqlcrudtest-4799
+      server_name: sqlcrudtest-5961
+      name: copydb
+      location: eastus
+      create_mode: copy
+      source_database_id: "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/Default-SQL-SouthEastAsia/providers/Microsoft.Sql/servers/tests
+                          vr/databases/testdb"
+
 '''
 
 RETURN = '''
