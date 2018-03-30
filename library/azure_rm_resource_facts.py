c660e5edf74573b4b2ac106c5b0695948e49295b
zikalino <zikalino@microsoft.com>
2018-03-30 09:21:10 +0800

    moved generic rest client to a separate file

--- library/azure_rm_resource_facts.py
+++ library/azure_rm_resource_facts.py
@@ -87,6 +87,7 @@ response:
 '''
 
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+from ansible.module_utils.azure_rm_common_rest import GenericRestClient
 
 try:
     from msrestazure.azure_exceptions import CloudError
@@ -206,49 +207,6 @@ class AzureRMResourceFacts(AzureRMModuleBase):
         response = self.mgmt_client.query(self.url, "get", query_parameters, header_parameters, None, self.status_code)
         return json.loads(response.text)
 
-
-class GenericRestClientConfiguration(AzureConfiguration):
-
-    def __init__(
-            self, credentials, subscription_id, base_url=None):
-
-        if credentials is None:
-            raise ValueError("Parameter 'credentials' must not be None.")
-        if subscription_id is None:
-            raise ValueError("Parameter 'subscription_id' must not be None.")
-        if not base_url:
-            base_url = 'https://management.azure.com'
-
-        super(GenericRestClientConfiguration, self).__init__(base_url)
-
-        self.add_user_agent('genericrestclient/1.0')
-        self.add_user_agent('Azure-SDK-For-Python')
-
-        self.credentials = credentials
-        self.subscription_id = subscription_id
-
-
-class GenericRestClient(object):
-
-    def __init__(self, credentials, subscription_id, base_url=None):
-        self.config = GenericRestClientConfiguration(credentials, subscription_id, base_url)
-        self._client = ServiceClient(self.config.credentials, self.config)
-        self.models = None
-
-    def query(self, url, method, query_parameters, header_parameters, body, expected_status_codes):
-        # Construct and send request
-        operation_config = {}
-
-        request = self._client.get(url, query_parameters)
-        response = self._client.send(request, header_parameters, body, **operation_config)
-
-        if response.status_code not in expected_status_codes:
-            exp = CloudError(response)
-            exp.request_id = response.headers.get('x-ms-request-id')
-            raise exp
-
-        return response
-        
 
 def main():
     AzureRMResourceFacts()
