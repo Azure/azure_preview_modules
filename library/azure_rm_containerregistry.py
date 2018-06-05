38f7c91062f24599d169920ac183a50848f392dd
zikalino <zikalino@microsoft.com>
2018-06-05 09:49:08 +0800

    undo changes

--- library/azure_rm_containerregistry.py
+++ library/azure_rm_containerregistry.py
@@ -412,9 +412,12 @@ class AzureRMContainerRegistry(AzureRMModuleBase):
     def containerregistry_mgmt_client(self):
         self.log('Getting container registry mgmt client')
         if not self._containerregistry_mgmt_client:
-            self._containerregistry_mgmt_client = self.get_mgmt_svc_client(ContainerRegistryManagementClient,
-                                                      base_url=self._cloud_environment.endpoints.resource_manager,
-                                                      version='2017-10-01')
+        if not self._containerregistry_mgmt_client:
+            self._containerregistry_mgmt_client = self.get_mgmt_svc_client(
+                ContainerRegistryManagementClient,
+                base_url=self._cloud_environment.endpoints.resource_manager,
+                api_version='2017-10-01'
+            )
 
         return self._containerregistry_mgmt_client
 
