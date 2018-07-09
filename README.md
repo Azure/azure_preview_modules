c8a9ecaf2656bad9f29b1402ebe52173c5b35e72
yungez <yungez@microsoft.com>
2018-07-09 14:24:48 +0800

    port integration branch to automerging upstream change

--- README.md
+++ README.md
@@ -1,100 +1,13 @@
-[![Travis CI](https://travis-ci.org/Azure/azure_preview_modules.svg?branch=master)](https://travis-ci.org/Azure/azure_preview_modules)
+This `integration` branch is for internal usage only. The purpose is to automate the workflow of merging Azure module related changes from Ansible repository to this repository. 
 
+The Travis CI has been setup to run the automation once every week. The CI can also be triggered manually in the Travis portal. 
 
-Azure.azure_preview_modules
-=========
+Steps to trigger the automation:
 
-This role is the most complete and includes all the latest Azure modules. The update and bug fix are done in a more timely manner than official Ansible release.
+- Go to travis [link](https://travis-ci.org/Azure/azure_preview_modules/branches) for `azure_preview_modules`.
+- Find `integration` branch and click latest build.
+- There is a `Restart Build` in the build detail page. 
 
-If you use Ansible for Azure resource provisioning purpose, you're strongly encouraged to install this role. 
+If there are new changes from Ansible repository, a new pull request will be created against `master` branch in `azure_preview_modules` repository. And the PR will trigger CI for testing. No changes, no PR.
 
-Prerequisite
-------------
-
-The usage of this playbook role assumes that you've already setup an Ansible environment for Azure. For details, please refer to Ansible tutorial [Getting Started with Azure](http://docs.ansible.com/ansible/latest/guide_azure.html) or [Install and configure Ansible](https://docs.microsoft.com/en-us/azure/virtual-machines/linux/ansible-install-configure). 
-
-
-Installation
-------------
-
-1. Install the role.
-
-  ``` bash
-  $ ansible-galaxy install Azure.azure_preview_modules
-  ```
-
-2. Upgrade Azure Python SDKs required by new Azure modules.
-
-  ``` bash
-  $ pip install -r ~/.ansible/roles/Azure.azure_preview_modules/files/requirements-azure.txt
-  ```
-    
-  &nbsp;&nbsp;&nbsp;&nbsp;or
-      
-  ``` bash
-  $ sudo pip install -r ~/.ansible/roles/Azure.azure_preview_modules/files/requirements-azure.txt
-  ```
-
-   Several reasons for installing Python SDKs are listed here.
-
-   - New module is added to the role and this module is for one new Azure resource, which is not included in existing Ansible releases yet. Corresponding SDK for this new resource needs to be installed.
-
-   - Newer versions of SDKs may introduce breaking API change. One specific working version should be installed here.
-
-   The required SDKs are listed in the *[~/files/requirements-azure.txt](files/requirements-azure.txt)* file. The `requirements-azure.txt` file can be found in the installed role folder, which is usually at `~/.ansible/roles/files` folder.
-    
-   The tricky part is the installation location, which has to be the same as where existing Azure Python SDKs are installed. If you meet any error when executing above command, please read below part carefully to double check the installation location. 
-
-   Taking Ubuntu for example, the existing SDKs may be located in folders like `/home/<your-user-name>/.local/lib/python2.7/site-packages` or `/usr/local/lib/python2.7/dist-packages`. The former is a user folder and the latter is a system folder, which requires sudo access. This depends on how you have installed `ansible`. In short, you should install the SDKs the same way as you installed `ansible` so that the SDKs are in the same `site-packages` folder.
-
-   On macOS, the existing SDKs may be located in folders like `/Users/<your-user-name>/Library/Python/2.7/lib/python/site-packages` or `/Library/Python/2.7/site-packages`. Like Ubuntu, the former is an user folder and the latter is a system folder.
-
-   One way to figure out the correct `site-packages` path is to check the details of existing packages, say `azure-mgmt-storage` by running below command.
-
-  ``` bash
-  $ pip show azure-mgmt-storage
-  ```
-
-   You can get the output like below. 
-
-  ``` bash
-  your-user-name@ansible:~$ pip show azure-mgmt-storage
-  Name: azure-mgmt-storage
-  Version: 1.5.0
-  Summary: Microsoft Azure Storage Management Client Library for Python
-  Home-page: https://github.com/Azure/azure-sdk-for-python
-  Author: Microsoft Corporation
-  Author-email: azpysdkhelp@microsoft.com
-  License: MIT License
-  Location: /home/<your-user-name>/.local/lib/python2.7/site-packages
-  Requires: azure-common, azure-mgmt-nspkg, msrestazure
-  ```
-
-From above information you can learn that the SDKs are installed in *user* `site-packages` folder and use `pip install` to install the listed packages. If the SDKs are installed in *system* path, use `sudo pip install` to install the listed packages.
-
-Role Variables
---------------
-
-No.
-
-Dependencies
-------------
-
-No dependencies on other roles.
-
-Example Playbook
-----------------
-
-    - hosts: localhost
-      roles:
-        - { role: Azure.azure_preview_modules }
-      tasks:
-      - name: create storage account
-        azure_rm_storageaccount:
-          resource_group: resourcegroupname
-          name: storagename
-          account_type: Standard_LRS
-
-License
--------
-MIT
+Repository owner can decide the next step, given the CI result of the new PR.
---
+++ main.py
@@ -0,0 +1,190 @@
+from github import Github
+import json
+import io
+import os
+import shutil
+from time import gmtime, strftime
+from git import Repo
+from git import Actor
+
+github_access_token = os.environ['GITHUB_ACCESS_TOKEN']
+
+config = json.load(open("./config.json"))
+ansible_repo_path = config["ansible_repo_path"]
+azure_repo_path = config["azure_repo_path"]
+sha_file_path = config["sha_file_path"]
+repo_mapping_file_path = config["repo_mapping_file_path"]
+
+local_azure_repo = None
+new_branch_name = None
+
+
+def get_latest_commit_sha(repo, branch, path):
+    commits = repo.get_commits(sha=branch, path=path)
+    page = commits.get_page(0)
+    if len(page) > 0:
+        return page[0].sha
+    else:
+        return None
+
+
+def clone_azure_repo():
+    print "Clone Azure repo to " + azure_repo_path
+    create_clean_dir(azure_repo_path)
+    global local_azure_repo
+    azure_repo = Repo()
+    local_azure_repo = azure_repo.clone_from("https://ansibleazurebot:" + github_access_token + "@github.com/Azure/azure_preview_modules.git", azure_repo_path)
+
+
+def has_new_content(repo, branch_name, log_file):
+    clone_azure_repo()
+    commit_history = json.load(open(log_file))
+    for path, sha in commit_history.iteritems():
+        new_sha = get_latest_commit_sha(repo, branch_name, path)
+        if sha != new_sha:
+            return True
+    return False
+
+
+def ansible_repo_has_new_content():
+    g = Github(github_access_token)
+    ansible_org = g.get_organization("ansible")
+    remote_ansible_repo = ansible_org.get_repo("ansible")
+    return has_new_content(remote_ansible_repo, config["ansible_default_branch"], sha_file_path)
+
+
+def save_back_to_json(data, file_name):
+    print "Save data to " + file_name
+    with io.open(file_name, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False))
+
+
+def create_clean_dir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+
+
+def clone_ansible_repo():
+    print "Clone Ansible repo to " + ansible_repo_path
+    create_clean_dir(ansible_repo_path)
+    ansible_repo = Repo()
+    ansible_repo.clone_from("https://github.com/ansible/ansible.git", ansible_repo_path)
+
+
+def copy(path):
+    full_path = os.path.join(ansible_repo_path, path)
+    if os.path.isdir(full_path):
+        copy_folder(path)
+    else:
+        copy_file(path)
+
+
+def get_joined_path(ansible_path):
+    repo_map = json.load(open(repo_mapping_file_path))
+    azure_path = repo_map[ansible_path]
+
+    src = os.path.join(ansible_repo_path, ansible_path)
+    dest = os.path.join(azure_repo_path, azure_path)
+
+    return src, dest
+
+
+def copy_file(path):
+    print "Copy file " + path
+    src, dest = get_joined_path(path)
+    shutil.copy(src, dest)
+
+
+def copy_folder(path):
+    print "Copy folder " + path
+    if config["special_src_folder_path"] in path:
+        copy_folder_specially(path)
+    else:
+        copy_folder_normally(path)
+
+
+def copy_folder_normally(path):
+    src, dest = get_joined_path(path)
+    shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
+
+def copy_folder_specially(path):
+    src, dest = get_joined_path(path)
+    shutil.rmtree(dest)
+    for dir_name in os.listdir(src):
+        src_dir_path = os.path.join(src, dir_name)
+        dest_dir_path = os.path.join(dest, dir_name)
+        if os.path.isdir(src_dir_path) and config["test_case_folder_prefix"] in dir_name:
+            shutil.copytree(src_dir_path, dest_dir_path)
+
+
+def check_out_new_branch():
+    global new_branch_name
+    new_branch_name = "integration-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+    print "Create new branch " + new_branch_name
+    local_azure_repo.git.checkout('HEAD', b=new_branch_name)
+
+
+def copy_changed_files():
+    print "Copy changes"
+    g = Github(github_access_token)
+    ansible_org = g.get_organization("ansible")
+    remote_ansible_repo = ansible_org.get_repo("ansible")
+    commit_history = json.load(open(sha_file_path))
+
+    changed = False
+    for path, sha in commit_history.iteritems():
+        new_sha = get_latest_commit_sha(remote_ansible_repo, "devel", path)
+        if sha != new_sha:
+            copy(path)
+            changed = True
+            commit_history[path]=new_sha
+
+    if changed:
+        save_back_to_json(commit_history, sha_file_path)
+
+
+def push_changes_to_remote():
+    print "Commit and push changes to remote"
+    local_azure_repo.git.add(A=True)
+    author = Actor(config["author"], config["mail"])
+    committer = Actor(config["author"], config["mail"])
+    local_azure_repo.index.commit("Merged changes of Azure modules from Ansible repo", author=author, committer=committer)
+    refspec = new_branch_name+":"+new_branch_name
+    local_azure_repo.remotes.origin.push(refspec=refspec)
+
+
+def send_pull_request():
+    print "Send pull request"
+    g = Github(github_access_token)
+    azure_org = g.get_organization("Azure")
+    remote_azure_repo = azure_org.get_repo("azure_preview_modules")
+    pr = remote_azure_repo.create_pull("[Automated Integration]Merge Azure module changes from Ansible repo",
+                                       config["alias"], config["azure_default_branch"], new_branch_name)
+    print "Created PR: " + pr.url
+
+
+def migrate_contents():
+    clone_ansible_repo()
+    check_out_new_branch()
+    copy_changed_files()
+    push_changes_to_remote()
+    send_pull_request()
+
+
+def main():
+    print "Starting..."
+
+    if not ansible_repo_has_new_content():
+        print "No new content. Exiting..."
+        return
+    else:
+        print "Migrate content from Ansible repo to Azure modules repo"
+
+    migrate_contents()
+
+
+if __name__ == "__main__":
+    main()
