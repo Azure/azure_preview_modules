4cbfb4d7fddfd25ff196ad35bac3a6c01655e90e
yungez <yungez@microsoft.com>
2018-10-08 15:46:44 +0800

    cdn endpoint test

--- library/azure_rm_cdnprofile.py
+++ library/azure_rm_cdnprofile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2018 Hai Cao, <t-haicao@microsoft.com>
+# Copyright (c) 2018 Hai Cao, <t-haicao@microsoft.com>, Yunge Zhu <yungez@microsoft.com>
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -14,10 +14,10 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: azure_rm_cdnprofile
-version_added: "2.7"
-short_description: Manage a CDN profile.
+version_added: "2.8"
+short_description: Manage a Azure CDN profile.
 description:
-    - Create, update and delete a CDN profile.
+    - Create, update and delete a Azure CDN profile.
 
 options:
     resource_group:
@@ -28,6 +28,19 @@ options:
         description:
             - Name of the CDN profile.
         required: true
+    location:
+        description:
+            - Valid azure location. Defaults to location of the resource group.
+    sku:
+        description:
+            - The pricing tier, defines a CDN provider, feature list and rate of the CDN profile.
+            - Detailed pricing can be find at U(https://azure.microsoft.com/en-us/pricing/details/cdn/)
+        choices:
+            - standard_verizon
+            - premium_verizon
+            - custom_verizon
+            - standard_akamai
+            - standard_chinacdn
     state:
         description:
             - Assert the state of the CDN profile. Use C(present) to create or update a CDN profile and C(absent) to delete it.
@@ -35,19 +48,6 @@ options:
         choices:
             - absent
             - present
-    location:
-        description:
-            - Valid azure location. Defaults to location of the resource group.
-    sku:
-        description:
-            - The pricing tier (defines a CDN provider, feature list and rate) of the CDN profile.
-            - Detailed pricing can be find at https://azure.microsoft.com/en-us/pricing/details/cdn/
-        choices:
-            - Standard_Verizon
-            - Premium_Verizon
-            - Custom_Verizon
-            - Standard_Akamai
-            - Standard_ChinaCdn
 
 extends_documentation_fragment:
     - azure
@@ -55,6 +55,7 @@ extends_documentation_fragment:
 
 author:
     - "Hai Cao <t-haicao@microsoft.com>"
+    - "Yunge Zhu <yungez@microsoft.com>"
 '''
 
 EXAMPLES = '''
@@ -73,26 +74,18 @@ EXAMPLES = '''
         state: absent
 '''
 RETURN = '''
-state:
+id:
     description: Current state of the CDN profile
     returned: always
     type: dict
     example:
             id: /subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourcegroups/cdntest/providers/Microsoft.Cdn/profiles/cdntest
-            location: WestUs
-            name: cdntest
-            provisioning_state: Succeeded
-            resource_state: Active
-            sku: Standard_Akamai
-            tags: {
-                Environment: Test
-            },
-            type: Microsoft.Cdn/profiles
 '''
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from azure.mgmt.cdn.models import Profile, Sku, ErrorResponseException
+    from azure.mgmt.cdn import CdnManagementClient
 except ImportError:
     # This is handled in azure_rm_common
     pass
@@ -109,6 +102,7 @@ def cdnprofile_to_dict(cdnprofile):
         provisioning_state=cdnprofile.provisioning_state,
         tags=cdnprofile.tags
     )
+
 
 class AzureRMCdnprofile(AzureRMModuleBase):
 
@@ -132,7 +126,7 @@ class AzureRMCdnprofile(AzureRMModuleBase):
             ),
             sku=dict(
                 type='str',
-                choices=['Standard_Verizon', 'Premium_Verizon', 'Custom_Verizon', 'Standard_Akamai', 'Standard_ChinaCdn']
+                choices=['standard_verizon', 'premium_verizon', 'custom_verizon', 'standard_akamai', 'standard_chinacdn']
             )
         )
 
@@ -143,23 +137,26 @@ class AzureRMCdnprofile(AzureRMModuleBase):
         self.tags = None
         self.sku = None
 
+        self.cdn_client = None
+
         required_if = [
             ('state', 'present', ['sku'])
         ]
 
-
         self.results = dict(changed=False)
 
         super(AzureRMCdnprofile, self).__init__(derived_arg_spec=self.module_arg_spec,
-                                                    supports_check_mode=True,
-                                                    supports_tags=True,
-                                                    required_if=required_if)
+                                                supports_check_mode=True,
+                                                supports_tags=True,
+                                                required_if=required_if)
 
     def exec_module(self, **kwargs):
         """Main module execution method"""
 
         for key in list(self.module_arg_spec.keys()) + ['tags']:
             setattr(self, key, kwargs[key])
+
+        self.cdn_client = self.get_cdn_client()
 
         to_be_updated = False
 
@@ -173,13 +170,12 @@ class AzureRMCdnprofile(AzureRMModuleBase):
 
             if not response:
                 self.log("Need to create the CDN profile")
-                
+
                 if not self.check_mode:
-                    self.results = self.create_cdnprofile()
-                    self.log("Creation done")
+                    new_response = self.create_cdnprofile()
+                    self.results['id'] = new_response['id']
 
                 self.results['changed'] = True
-                return self.results
 
             else:
                 self.log('Results : {0}'.format(response))
@@ -193,21 +189,21 @@ class AzureRMCdnprofile(AzureRMModuleBase):
                     self.log("Need to update the CDN profile")
 
                     if not self.check_mode:
-                        self.results = self.update_cdnprofile()
-                        self.log("Update done")
+                        new_response = self.update_cdnprofile()
+                        self.results['id'] = new_response['id']
 
                     self.results['changed'] = True
-                    return self.results
 
-        elif self.state == 'absent' and response:
-            self.log("Need to delete the CDN profile")
-            self.results['changed'] = True
+        elif self.state == 'absent':
+            if not response:
+                self.fail("CDN profile {0} not exists.".format(self.name))
+            else:
+                self.log("Need to delete the CDN profile")
+                self.results['changed'] = True
 
-            if not self.check_mode:
-                self.delete_cdnprofile()
-                self.log("CDN profile deleted")
-
-            return self.results
+                if not self.check_mode:
+                    self.delete_cdnprofile()
+                    self.results['id'] = response['id']
 
         return self.results
 
@@ -225,13 +221,20 @@ class AzureRMCdnprofile(AzureRMModuleBase):
             tags=self.tags
         )
 
+        import uuid
+        xid = str(uuid.uuid1())
+
         try:
-            poller = self.cdn_client.profiles.create(self.resource_group, self.name, parameters)
+            poller = self.cdn_client.profiles.create(self.resource_group,
+                                                     self.name,
+                                                     parameters,
+                                                     custom_headers={'x-ms-client-request-id': xid}
+                                                     )
             response = self.get_poller_result(poller)
             return cdnprofile_to_dict(response)
         except ErrorResponseException as exc:
             self.log('Error attempting to create Azure CDN profile instance.')
-            self.fail("Error creating Azure CDN profile instance: {0}".format(exc.message))
+            self.fail("Error creating Azure CDN profile instance: {0}.\n Request id: {1}".format(exc.message, xid))
 
     def update_cdnprofile(self):
         '''
@@ -282,6 +285,13 @@ class AzureRMCdnprofile(AzureRMModuleBase):
         except ErrorResponseException:
             self.log('Did not find the CDN profile.')
             return False
+
+    def get_cdn_client(self):
+        if not self.cdn_client:
+            self.cdn_client = self.get_mgmt_svc_client(CdnManagementClient,
+                                                       base_url=self._cloud_environment.endpoints.resource_manager,
+                                                       api_version='2017-04-02')
+        return self.cdn_client
 
 
 def main():
