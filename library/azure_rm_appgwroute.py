c12dcb63db83a769c6ca7f145bad49b21c9f416c
zikalino <zikalino@microsoft.com>
2018-11-15 16:16:27 +0800

    fixed route and removed outdated test

--- library/azure_rm_appgwroute.py
+++ library/azure_rm_appgwroute.py
@@ -93,7 +93,7 @@ from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from msrestazure.azure_exceptions import CloudError
-    from msrestazure.azure_operation import AzureOperationPoller
+    from msrest.polling import LROPoller
     from azure.mgmt.network import NetworkManagementClient
     from msrest.serialization import Model
 except ImportError:
@@ -264,7 +264,7 @@ class AzureRMRoutes(AzureRMModuleBase):
                                                                 route_table_name=self.route_table_name,
                                                                 route_name=self.route_name,
                                                                 route_parameters=self.parameters)
-            if isinstance(response, AzureOperationPoller):
+            if isinstance(response, LROPoller):
                 response = self.get_poller_result(response)
 
         except CloudError as exc:
