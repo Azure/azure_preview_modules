d07c5a4ffdd97f6367ceb72e2787165265e9c0bb
zikalino <zikalino@microsoft.com>
2019-01-17 15:13:42 +0800

    updating artifacts source

--- library/azure_rm_devtestlabartifactsource.py
+++ library/azure_rm_devtestlabartifactsource.py
@@ -34,9 +34,6 @@ options:
         description:
             - The name of the artifact source.
         required: True
-    location:
-        description:
-            - The location of the resource.
     display_name:
         description:
             - "The artifact source's display name."
@@ -47,7 +44,7 @@ options:
         description:
             - "The artifact source's type."
         choices:
-            - 'vso_git'
+            - 'vso'
             - 'github'
     folder_path:
         description:
@@ -61,9 +58,9 @@ options:
     security_token:
         description:
             - The security token to authenticate to the artifact source.
-    status:
+    enabled:
         description:
-            - "Indicates if the artifact source is enabled (values: Enabled, Disabled). Possible values include: 'Enabled', 'Disabled'"
+            - Indicates whether the artifact source is enabled.
         type: bool
     state:
       description:
@@ -85,11 +82,14 @@ author:
 
 EXAMPLES = '''
   - name: Create (or update) DevTest Labs Artifacts Source
-    azure_rm_devtestlabsartifactsource:
-      resource_group: NOT FOUND
-      lab_name: NOT FOUND
-      name: NOT FOUND
-      status: status
+    azure_rm_devtestlabartifactsource:
+      resource_group: myrg
+      lab_name: mylab
+      name: myartifacts
+      uri: https://github.com/myself/myrepo.git
+      source_type: github
+      folder_path: /
+      security_token: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 '''
 
 RETURN = '''
@@ -98,13 +98,13 @@ id:
         - The identifier of the resource.
     returned: always
     type: str
-    sample: id
-status:
+    sample: /subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourcegroups/myrg/providers/microsoft.devtestlab/labs/mylab/artifactsources/myartifacts
+is_enabled:
     description:
-        - "Indicates if the artifact source is enabled (values: Enabled, Disabled). Possible values include: 'Enabled', 'Disabled'"
+        - Indicates whether the artifact source is enabled.
     returned: always
-    type: str
-    sample: status
+    type: bool
+    sample: true
 '''
 
 import time
@@ -143,9 +143,6 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
                 type='str',
                 required=True
             ),
-            location=dict(
-                type='str'
-            ),
             display_name=dict(
                 type='str'
             ),
@@ -154,7 +151,7 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             ),
             source_type=dict(
                 type='str',
-                choices=['vso_git',
+                choices=['vso',
                          'github']
             ),
             folder_path=dict(
@@ -169,7 +166,7 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             security_token=dict(
                 type='str'
             ),
-            status=dict(
+            is_enabled=dict(
                 type='bool'
             ),
             state=dict(
@@ -189,9 +186,15 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
         self.state = None
         self.to_do = Actions.NoAction
 
+        required_if = [
+            ('state', 'present', [
+             'source_type', 'uri', 'security_token'])
+        ]
+
         super(AzureRMDevTestLabArtifactsSource, self).__init__(derived_arg_spec=self.module_arg_spec,
                                                                    supports_check_mode=True,
-                                                                   supports_tags=True)
+                                                                   supports_tags=True,
+                                                                   required_if=required_if)
 
     def exec_module(self, **kwargs):
         """Main module execution method"""
@@ -202,18 +205,20 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             elif kwargs[key] is not None:
                 self.artifact_source[key] = kwargs[key]
 
-        dict_camelize(self.artifact_source, ['source_type'], True)
-        dict_map(self.artifact_source, ['source_type'], {'github': 'GitHub'})
-        dict_map(self.artifact_source, ['status'], {True: 'Enabled', False: 'Disabled'})
+        if self.artifact_source['source_type'] == 'github':
+            self.artifact_source['source_type'] = 'GitHub'
+        elif self.artifact_source['source_type'] == 'vso':
+            self.artifact_source['source_type'] = 'VsoGit'
+
+        if self.artifact_source.get('status') is not None:
+            self.artifact_source['status'] = 'Enabled' if self.artifact_source.get('status') else 'Disabled'
 
         response = None
 
         self.mgmt_client = self.get_mgmt_svc_client(DevTestLabsClient,
                                                     base_url=self._cloud_environment.endpoints.resource_manager)
 
-        resource_group = self.get_resource_group(self.resource_group)
-
-        old_response = self.get_devtestlabsartifactssource()
+        old_response = self.get_devtestlabartifactssource()
 
         if not old_response:
             self.log("DevTest Labs Artifacts Source instance doesn't exist")
@@ -226,31 +231,57 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             if self.state == 'absent':
                 self.to_do = Actions.Delete
             elif self.state == 'present':
-                if (not default_compare(self.artifact_source, old_response, '', self.results)):
+                self.results['old_response'] = old_response
+
+                if self.artifact_source.get('display_name') is not None:
+                    if self.artifact_source.get('display_name') != old_response.get('display_name'):
+                        self.to_do = Actions.Update
+                else:
+                    self.artifact_source['display_name'] = old_response.get('display_name')
+
+                if self.artifact_source.get('source_type').lower() != old_response.get('source_type').lower():
                     self.to_do = Actions.Update
+
+                if self.artifact_source.get('uri').lower() != old_response.get('uri'):
+                    self.to_do = Actions.Update
+
+                if self.artifact_source.get('branch_ref') is not None:
+                    if self.artifact_source.get('branch_ref') != old_response.get('branch_ref'):
+                        self.to_do = Actions.Update
+                else:
+                    self.artifact_source['branch_ref'] = old_response.get('branch_ref')
+
+                if self.artifact_source.get('status') is not None:
+                    if self.artifact_source.get('status') != old_response.get('status'):
+                        self.to_do = Actions.Update
+                else:
+                    self.artifact_source['status'] = old_response.get('status')
+
+                if self.artifact_source.get('folder_path') is not None:
+                    if self.artifact_source.get('folder_path') != old_response.get('folder_path'):
+                        self.to_do = Actions.Update
+                else:
+                    self.artifact_source['folder_path'] = old_response.get('folder_path')
+
+                if self.artifact_source.get('arm_template_folder_path') is not None:
+                    if self.artifact_source.get('arm_template_folder_path') != old_response.get('arm_template_folder_path'):
+                        self.to_do = Actions.Update
+                else:
+                    self.artifact_source['arm_template_folder_path'] = old_response.get('arm_template_folder_path')
 
         if (self.to_do == Actions.Create) or (self.to_do == Actions.Update):
             self.log("Need to Create / Update the DevTest Labs Artifacts Source instance")
-
-            if self.check_mode:
-                self.results['changed'] = True
-                return self.results
-
-            response = self.create_update_devtestlabsartifactssource()
-
             self.results['changed'] = True
+            if self.check_mode:
+                return self.results
+            response = self.create_update_devtestlabartifactssource()
             self.log("Creation / Update done")
         elif self.to_do == Actions.Delete:
             self.log("DevTest Labs Artifacts Source instance deleted")
             self.results['changed'] = True
-
             if self.check_mode:
                 return self.results
-
-            self.delete_devtestlabsartifactssource()
-            # This currently doesnt' work as there is a bug in SDK / Service
-            if isinstance(response, LROPoller) or isinstance(response, AzureOperationPoller):
-                response = self.get_poller_result(response)
+            self.delete_devtestlabartifactssource()
         else:
             self.log("DevTest Labs Artifacts Source instance unchanged")
             self.results['changed'] = False
@@ -259,11 +290,11 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
         if self.state == 'present':
             self.results.update({
                 'id': response.get('id', None),
-                'status': response.get('status', None)
-                })
+                'is_enabled': (response.get('status', None).lower() == 'enabled')
+            })
         return self.results
 
-    def create_update_devtestlabsartifactssource(self):
+    def create_update_devtestlabartifactssource(self):
         '''
         Creates or updates DevTest Labs Artifacts Source with the specified configuration.
 
@@ -284,7 +315,7 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             self.fail("Error creating the DevTest Labs Artifacts Source instance: {0}".format(str(exc)))
         return response.as_dict()
 
-    def delete_devtestlabsartifactssource(self):
+    def delete_devtestlabartifactssource(self):
         '''
         Deletes specified DevTest Labs Artifacts Source instance in the specified subscription and resource group.
 
@@ -301,7 +332,7 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
 
         return True
 
-    def get_devtestlabsartifactssource(self):
+    def get_devtestlabartifactssource(self):
         '''
         Gets the properties of the specified DevTest Labs Artifacts Source.
 
@@ -322,79 +353,6 @@ class AzureRMDevTestLabArtifactsSource(AzureRMModuleBase):
             return response.as_dict()
 
         return False
-
-
-def default_compare(new, old, path, result):
-    if new is None:
-        return True
-    elif isinstance(new, dict):
-        if not isinstance(old, dict):
-            result['compare'] = 'changed [' + path + '] old dict is null'
-            return False
-        for k in new.keys():
-            if not default_compare(new.get(k), old.get(k, None), path + '/' + k, result):
-                return False
-        return True
-    elif isinstance(new, list):
-        if not isinstance(old, list) or len(new) != len(old):
-            result['compare'] = 'changed [' + path + '] length is different or null'
-            return False
-        if isinstance(old[0], dict):
-            key = None
-            if 'id' in old[0] and 'id' in new[0]:
-                key = 'id'
-            elif 'name' in old[0] and 'name' in new[0]:
-                key = 'name'
-            else:
-                key = list(old[0])[0]
-            new = sorted(new, key=lambda x: x.get(key, None))
-            old = sorted(old, key=lambda x: x.get(key, None))
-        else:
-            new = sorted(new)
-            old = sorted(old)
-        for i in range(len(new)):
-            if not default_compare(new[i], old[i], path + '/*', result):
-                return False
-        return True
-    else:
-        if path == '/location':
-            new = new.replace(' ', '').lower()
-            old = new.replace(' ', '').lower()
-        if new == old:
-            return True
-        else:
-            result['compare'] = 'changed [' + path + '] ' + str(new) + ' != ' + str(old)
-            return False
-
-
-def dict_camelize(d, path, camelize_first):
-    if isinstance(d, list):
-        for i in range(len(d)):
-            dict_camelize(d[i], path, camelize_first)
-    elif isinstance(d, dict):
-        if len(path) == 1:
-            old_value = d.get(path[0], None)
-            if old_value is not None:
-                d[path[0]] = _snake_to_camel(old_value, camelize_first)
-        else:
-            sd = d.get(path[0], None)
-            if sd is not None:
-                dict_camelize(sd, path[1:], camelize_first)
-
-
-def dict_map(d, path, map):
-    if isinstance(d, list):
-        for i in range(len(d)):
-            dict_map(d[i], path, map)
-    elif isinstance(d, dict):
-        if len(path) == 1:
-            old_value = d.get(path[0], None)
-            if old_value is not None:
-                d[path[0]] = map.get(old_value, old_value)
-        else:
-            sd = d.get(path[0], None)
-            if sd is not None:
-                dict_map(sd, path[1:], map)
 
 
 def main():
