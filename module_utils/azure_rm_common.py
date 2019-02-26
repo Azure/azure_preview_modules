b5d9dfa775e6ec3ef567a42c843d4e91c5ea2eab
zikalino <zikalino@microsoft.com>
2019-02-26 12:27:38 +0800

    missing required lib fix

--- module_utils/azure_rm_common.py
+++ module_utils/azure_rm_common.py
@@ -13,7 +13,12 @@ import json
 
 from os.path import expanduser
 
-from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+try:
+    from ansible.module_utils.basic import missing_required_lib
+except Exception:
+    def missing_required_lib(msg, reason=None, url=None):
+        return msg
+
 try:
     from ansible.module_utils.ansible_release import __version__ as ANSIBLE_VERSION
 except Exception:
