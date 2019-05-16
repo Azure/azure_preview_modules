a79ca79da35ebab8d71a1fc2ec32df815e7be92a
yuwzho <yuwzho@microsoft.com>
2019-05-16 11:36:03 +0800

    add back notification

--- module_utils/azure_rm_common.py
+++ module_utils/azure_rm_common.py
@@ -1358,7 +1358,7 @@ class AzureRMAuth(object):
 
     def log(self, msg, pretty_print=False):
         pass
-        # Use only during module development!
+        # Use only during module development
         # if self.debug:
         #     log_file = open('azure_rm.log', 'a')
         #     if pretty_print:
